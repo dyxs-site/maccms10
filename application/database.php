a6b792c03969403c53323cbd017f7b65aa5c259f
电影先生 <mail@dyxs.site>
2023-12-22 15:10:19 +0800

    添加海螺模板ConchM005 V3.1

--- application/database.php
+++ application/database.php
@@ -12,7 +12,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '172.28.0.1',
+    'hostname'        => '172.20.0.3',
     // 数据库名
     'database'        => 'maccms',
     // 用户名
