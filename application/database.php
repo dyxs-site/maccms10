1ebfbf7524c677dcc9d2fd193a1b084769336fd4
电影先生 <mail@dyxs.site>
2023-12-27 09:05:30 +0000

    fix

--- application/database.php
+++ application/database.php
@@ -18,7 +18,7 @@ return [
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '123456',
+    'password'        => 'maccmsv10%dianyingxs.cc',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
