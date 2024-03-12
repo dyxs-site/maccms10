d0ca29b8f7109c477fa308a9859a08841df455ea
电影先生 <mail@dyxs.site>
2024-03-12 03:02:01 +0000

    服务器同步

--- application/extra/cjfaves.php
+++ application/extra/cjfaves.php
@@ -1,4 +1,51 @@
 <?php
 return array (
-
+  0 => 
+  array (
+    'name' => '闪电资源站',
+    'rema' => '全网秒播,资源齐全',
+    'flag' => 'shandianzy',
+    'apis' => 'https://sdzyapi.com/api.php/provide/vod/from/sdm3u8/at/json/',
+    'zylink' => 'https://www.shan05.com',
+    'coll' => '闪电播放,sdm3u8,1207,1',
+    'type' => '2',
+    'filter' => '0',
+    'filter_from' => '',
+    'opt' => '0',
+    'sync_pic_opt' => '0',
+    'tips' => '<span class="layui-badge layui-bg-green">我的收藏</span>',
+    'collect_mid' => 1,
+  ),
+  1 => 
+  array (
+    'name' => '无尽资源站',
+    'rema' => '国内CDN,全网秒播,资源丰富',
+    'flag' => 'wujinzy',
+    'apis' => 'https://api.wujinapi.cc/api.php/provide/vod/from/wjm3u8/at/json/',
+    'zylink' => 'http://www.wujinzy.net/',
+    'coll' => '无尽①,wjm3u8,1194,1',
+    'type' => '2',
+    'filter' => '0',
+    'filter_from' => '',
+    'opt' => '0',
+    'sync_pic_opt' => '0',
+    'tips' => '<span class="layui-badge layui-bg-green">我的收藏</span>',
+    'collect_mid' => 1,
+  ),
+  2 => 
+  array (
+    'name' => '红牛资源站',
+    'rema' => '全网秒播,国内CDN,资源齐全',
+    'flag' => 'hongniuzy',
+    'apis' => 'https://www.hongniuzy2.com/api.php/provide/vod/from/hnm3u8/at/json/',
+    'zylink' => 'https://hongniuzy.net/help/',
+    'coll' => '红牛在线,hnm3u8,1193,1',
+    'type' => '2',
+    'filter' => '0',
+    'filter_from' => '',
+    'opt' => '0',
+    'sync_pic_opt' => '0',
+    'tips' => '<span class="layui-badge layui-bg-green">我的收藏</span>',
+    'collect_mid' => 1,
+  ),
 );
--- application/extra/timming.php
+++ application/extra/timming.php
@@ -2,7 +2,7 @@
 return array (
   'hongniuzyzanzhu01' => 
   array (
-    '__token__' => 'cebbbc7dcd553d1eb7e5d1e2ff76e2a8',
+    '__token__' => '29a5b20e1e3294ac1ff7881510e0af7b',
     'status' => '1',
     'name' => 'hongniuzyzanzhu01',
     'des' => '当日采集：红牛资源站【全网秒播,国内CDN,资源齐全】',
@@ -10,7 +10,7 @@ return array (
     'param' => 'ac=cj&h=24&cjflag=hongniuzy&cjurl=https%3A%2F%2Fwww.hongniuzy2.com%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fhnm3u8%2Fat%2Fjson%2F',
     'weeks' => '1,2,3,4,5,6,0',
     'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-    'runtime' => 1709471147,
+    'runtime' => 1710208954,
   ),
   'hongniuzyzanzhu01week' => 
   array (
@@ -22,6 +22,18 @@ return array (
     'param' => 'ac=cj&h=168&cjflag=hongniuzy&cjurl=https%3A%2F%2Fwww.hongniuzy2.com%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fhnm3u8%2Fat%2Fjson%2F',
     'weeks' => '1',
     'hours' => '00',
-    'runtime' => 1703230504,
+    'runtime' => 1709481772,
+  ),
+  'wujinzyzanzhu02' => 
+  array (
+    '__token__' => '5c3edc93051acef6d148f58c3175ab58',
+    'status' => '1',
+    'name' => 'wujinzyzanzhu02',
+    'des' => '当日采集：无尽资源站【国内CDN,全网秒播,资源丰富】',
+    'file' => 'collect',
+    'param' => 'ac=cj&h=24&cjflag=wujinzy&cjurl=https%3A%2F%2Fapi.wujinapi.cc%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fwjm3u8%2Fat%2Fjson%2F',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1710209146,
   ),
 );
