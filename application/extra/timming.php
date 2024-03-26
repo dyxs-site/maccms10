829b43fac2f0ca59beb347096de3267039c928d2
电影先生 <mail@dyxs.site>
2024-03-26 06:51:00 +0000

    服务器配置同步

--- application/extra/timming.php
+++ application/extra/timming.php
@@ -10,7 +10,7 @@ return array (
     'param' => 'ac=cj&h=24&cjflag=hongniuzy&cjurl=https%3A%2F%2Fwww.hongniuzy2.com%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fhnm3u8%2Fat%2Fjson%2F',
     'weeks' => '1,2,3,4,5,6,0',
     'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-    'runtime' => 1711361700,
+    'runtime' => 1711433700,
   ),
   'hongniuzyzanzhu01week' => 
   array (
@@ -34,7 +34,7 @@ return array (
     'param' => 'ac=cj&h=24&cjflag=wujinzy&cjurl=https%3A%2F%2Fapi.wujinapi.cc%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fwjm3u8%2Fat%2Fjson%2F',
     'weeks' => '1,2,3,4,5,6,0',
     'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-    'runtime' => 1711361400,
+    'runtime' => 1711433401,
   ),
   'suoniziyuan' => 
   array (
@@ -46,7 +46,7 @@ return array (
     'param' => 'https://dianyingxs.cc/xwadmin.php/admin/collect/api.html?ac=cj&cjflag=5b1b3247447b252856ec0aa15a80a5aa&cjurl=https%3A%2F%2Fsuoniapi.com%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fsnm3u8%2Fat%2Fxml%2F&h=24&t=&ids=&wd=&type=1&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&filter_year=&param=',
     'weeks' => '1,2,3,4,5,6,0',
     'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-    'runtime' => 1711361100,
+    'runtime' => 1711433100,
   ),
   'leshiziyuan' => 
   array (
@@ -59,6 +59,6 @@ return array (
     'param' => 'https://dianyingxs.cc/xwadmin.php/admin/collect/api.html?ac=cj&cjflag=4486d84af0e08af391e823fdd07a049d&cjurl=https%3A%2F%2Fleshiapi.com%2Fapi.php%2Fprovide%2Fvod%2Fat%2Fxml&h=24&t=&ids=&wd=&type=1&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&filter_year=&param=',
     'weeks' => '1,2,3,4,5,6,0',
     'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-    'runtime' => 1711360860,
+    'runtime' => 1711432861,
   ),
 );
