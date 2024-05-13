58a73defad957e67ef9d1a1d6772d21f8633ee96
电影先生 <mail@dyxs.site>
2024-05-13 14:59:24 +0800

    服务器同步

--- application/extra/maccms.php
+++ application/extra/maccms.php
@@ -33,8 +33,7 @@ return array (
     'mob_status' => '0',
     'mob_template_dir' => 'DYXS2',
     'mob_html_dir' => 'html',
-    'site_tj' => '<meta name="shenma-site-verification" content="d3bcebf5bd870060f28ab04bf9cc5805_1711425425">
-<script>
+    'site_tj' => '<script>
 (function(){
 var el = document.createElement("script");
 el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?ed3d9ff078b4c4b76d9802b12af1abd05f45fa2bca5d2bc46b17b3360fb41dae3d72cd14f8a76432df3935ab77ec54f830517b3cb210f7fd334f50ccb772134a";
@@ -57,16 +56,7 @@ s.parentNode.insertBefore(el, s);
 })();
    
 
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f46a2caaf84b781dc103b13289d90383";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-    
-    
-   let u="https://api.cgyx.tv:66",token="f7dd072abb9af17700b0b8a1bd8b8e1dc43e13838257436c31c30f4495aba5a79382ef3b36808ab90351f85de968be49838554c9b1e809e46631bdd330fc2189";var cltj = document.createElement("script");cltj.src = u + "/tj/tongji.js?v=1.3";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(cltj,s);
+!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3IBpiUGj8mdnRuE6",ck:"3IBpiUGj8mdnRuE6",autoTrack:true,screenRecord:true});
 
 </script>',
     'site_status' => '1',
@@ -627,8 +617,8 @@ plotdetail/<id>   => plot/detail',
     'widthpop' => '0',
     'heightpop' => '600',
     'second' => '5',
-    'prestrain' => '//hao.tools/static/player/prestrain.html',
-    'buffer' => '//hao.tools/static/player/loading.html',
+    'prestrain' => '//dyxs.site/static/player/prestrain.html',
+    'buffer' => '//dyxs.site/static/player/loading.html',
     'parse' => '',
     'autofull' => '0',
     'showtop' => '1',
